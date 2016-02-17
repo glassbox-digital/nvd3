3c28380faf56ebbaa97c1ca45427c5bfb85ad69f
nir shilon <shilon5@gmail.com>
2016-02-17 12:24:43 +0200

    fix tooltip offset

--- build/nv.d3.js
+++ build/nv.d3.js
@@ -558,8 +558,8 @@ nv.models.tooltip = function() {
      */
     var position = function() {
         return {
-            left: d3.event !== null ? d3.event.clientX : 0,
-            top: d3.event !== null ? d3.event.clientY : 0
+            left: d3.event !== null ? d3.event.offsetX : 0,
+            top: d3.event !== null ? d3.event.offsetY : 0
         };
     };
 
