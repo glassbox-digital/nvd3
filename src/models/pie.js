fab794aacd3a9fe4917e40d8f0e727111ac602de
nir shilon <shilon5@gmail.com>
2016-03-09 10:21:19 +0200

    pie: don't add selected class on slice w/o value

--- src/models/pie.js
+++ src/models/pie.js
@@ -229,7 +229,7 @@ nv.models.pie = function() {
             ae.append('path').attr('class', 'nv-check')
                 .attr('d', 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z');
 
-            slices.classed('selected', function(d){ return d.data.selected; })
+            slices.classed('selected', function(d){ return d.value > 0 && d.data.selected; })
 
             donutInfo();
 
