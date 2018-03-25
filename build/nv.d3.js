ed3704dda09a1895f9dd3f09921273a3b145dde9
nir@home <nir.shilon@clarisite.com>
2018-03-25 10:54:31 +0300

    legend check mark - EDGE

--- build/nv.d3.js
+++ build/nv.d3.js
@@ -1,4 +1,4 @@
-/* nvd3 version 1.8.1-dev (https://github.com/novus/nvd3) 2018-03-16 */
+/* nvd3 version 1.8.1-dev (https://github.com/novus/nvd3) 2018-03-25 */
 (function(){
 
 // set up main nv object
@@ -7895,6 +7895,7 @@ nv.models.multiHistoricalBarChart = function () {
                 if (!updateState) {
                     seriesEnter.append('path')
                         .attr('class', 'nv-check')
+                        .attr('transform', 'translate(-12px, -12px)scale(.9)')
                         .attr('d', 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z');
                 }
 
