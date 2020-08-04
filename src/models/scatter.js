bd273f290f683aaff397f614a4066e881cc6c626
e1adn <elad.noy@glassboxdigital.com>
2020-08-04 19:01:49 +0300

    GLAS30-9677 - Anomaly Detection > Anomaly Points are moving out of place after resizing the page

--- src/models/scatter.js
+++ src/models/scatter.js
@@ -371,8 +371,15 @@ nv.models.scatter = function() {
             }
 
             needsUpdate = true;
-            var groups = wrap.select('.nv-groups').selectAll('.nv-group')
-                .data(function(d) { return d }, function(d) { return d.key });
+
+            var groupElements = wrap.select('.nv-groups').selectAll('.nv-group');
+
+            groupElements.forEach(function(g){
+                groupElements.selectAll('.nv-alert').remove();
+            });
+
+            var groups = groupElements.data(function(d) { return d }, function(d) { return d.key });
+
             groups.enter().append('g')
                 .style('stroke-opacity', 1e-6)
                 .style('fill-opacity', 1e-6);
